146225f4bbd76de98cc2fc4bbd8adb7a78cecbfc
clavera2 <alfred.claver2@mail.dcu.ie>
2025-10-27 15:40:33 +0000

    added java doc-comments

--- src/main/java/com/clavera2/http/HttpRequest.java
+++ src/main/java/com/clavera2/http/HttpRequest.java
@@ -3,6 +3,17 @@ package com.clavera2.http;
 import java.util.Map;
 import java.util.HashMap;
 
+
+/**
+ * This class represents a Http Request that can be sent from a client to a server.
+ * It conforms to the rfc specification of the HyperText Transfer Protocol. A http request
+ * object is built using {@link HttpRequest.RequestBuilder} instance; conforming to the 
+ * builder pattern. A http object consist of the following:
+ * - A {@link Method} which could be either GET, POST, PUT, PATCH or DELETE.
+ * - A header which is stored as a hashmap.
+ * - A body field containing message to be sent to the server
+ * 
+ */
 public class HttpRequest {
     private static final String VERSION = "1.0";
 
